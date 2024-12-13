1ab05fe22e54de865f359520385af676f58abe5e
Henrique <henriqueferreira0320@gmail.com>
2024-12-13 11:54:42 -0300

    Mostrando o body do template ao selecionar

--- app/Http/Requests/CampaignStoreRequest.php
+++ app/Http/Requests/CampaignStoreRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Template;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CampaignStoreRequest extends FormRequest
@@ -55,6 +56,11 @@ class CampaignStoreRequest extends FormRequest
                 }
             }
         // --
+
+        if($templateId = $session['template_id'] && blank($session['body'])){
+            $template = Template::find($templateId);
+            $session['body'] = $template->body;
+        }
         
         
         session()->put('campaigns::create', $session);
