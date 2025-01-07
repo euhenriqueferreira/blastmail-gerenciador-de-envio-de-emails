9dfebf11b653fffd511a0726d9e6ff38ed476214
Henrique <henriqueferreira0320@gmail.com>
2025-01-07 13:19:26 -0300

    Instalação do Pest

---
+++ tests/Feature/ExamplePestTest.php
@@ -0,0 +1,11 @@
+<?php
+
+test('testando tela de login', function(){
+    $this->get('/login')
+        ->assertOk();
+});
+
+it('should be able to login', function(){
+    $this->get('/login')
+    ->assertOk();
+});
