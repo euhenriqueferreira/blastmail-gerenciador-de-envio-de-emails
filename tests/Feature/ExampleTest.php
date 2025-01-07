e00e6845dde51424a079b70a137b83896478a1c3
Henrique <henriqueferreira0320@gmail.com>
2025-01-07 10:52:34 -0300

    Início com PHP Unit, Testando a Criação de Email List

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,11 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+namespace Tests\Feature;
 
-    $response->assertStatus(200);
-});
+use Tests\TestCase;
+
+class ExampleTest extends TestCase{
+    public function test_example2(){
+        $this->assertDatabaseCount('users', 2);
+    }
+}
